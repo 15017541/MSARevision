893f9ebb4ba602f258d7deb7e81e486c5c139558
jason-lim-rp <jason_lim@rp.edu.sg>
2017-06-14 15:32:17 +0800

    2nd update

--- app/src/main/java/com/myapplicationdev/android/msarevision/FragTwo.java
+++ app/src/main/java/com/myapplicationdev/android/msarevision/FragTwo.java
@@ -1,4 +1,4 @@
-package layout;
+package com.myapplicationdev.android.msarevision;
 
 
 import android.os.Bundle;
@@ -6,8 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.myapplicationdev.android.msarevision.R;
 
 /**
  * A simple {@link Fragment} subclass.
